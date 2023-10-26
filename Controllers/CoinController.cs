d4f5b95ce185bce0fdac06f95ed23bcae55ed8a3
Pedro Kalva Junior <pedro.kalva@gmail.com>
2023-10-25 21:25:27 -0300

    Adding coin controller with http client

---
+++ Controllers/CoinController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _angularJS.Controllers
+{
+    public class CoinController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public CoinController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://api.coingecko.com/api/v3/");
+            _httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync("api/resource");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+
+
+
+                    // Deserialize the JSON data into a model class (if applicable)
+                    // Example: var result = JsonConvert.DeserializeObject<MyModel>(data);
+
+                    return View("Index", data);
+                }
+
+                return View();
+            }
+            catch (Exception e)
+            {
+
+                return View("Error");
+            }     
+        }
+    }
+}
